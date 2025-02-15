53e5484d2bd3718086ba93cd805c4ee45dd3996e
Nathan Wick <nathanwick2001@gmail.com>
2025-02-15 17:08:59 -0500

    Fix Initialize Database

--- source/utilities/initialize-database.ts
+++ source/utilities/initialize-database.ts
@@ -1,16 +1,16 @@
-import { applicationConfiguration } from '../start-application.js';
+import { database } from '../start-application.js';
 import { initializeDashboardConfiguration } from '../database-models/dashboard-configuration.js';
 import { initializePreferences } from '../database-models/preferences.js';
 import { initializeSession } from '../database-models/session.js';
 import { initializeUser } from '../database-models/user.js';
 
 const initializeDatabase = async () => {
-	initializeDashboardConfiguration(applicationConfiguration.database);
-	initializePreferences(applicationConfiguration.database);
-	initializeSession(applicationConfiguration.database);
-	initializeUser(applicationConfiguration.database);
+	initializeDashboardConfiguration(database);
+	initializePreferences(database);
+	initializeSession(database);
+	initializeUser(database);
 
-	await applicationConfiguration.database.sync({ alter: true });
+	await database.sync({ alter: true });
 };
 
 export default initializeDatabase;
