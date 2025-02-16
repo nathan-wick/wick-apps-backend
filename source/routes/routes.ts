13bdb64a4540e72d9c15b29c840e2ddb7dbb26d0
Nathan Wick <nathanwick2001@gmail.com>
2025-02-15 19:14:35 -0500

    Sort Route Imports

--- source/routes/routes.ts
+++ source/routes/routes.ts
@@ -1,3 +1,11 @@
+/* eslint-disable sort-imports */
+import { Router } from 'express';
+
+export const authenticationRoute = Router();
+export const dashboardConfigurationRoute = Router();
+export const preferencesRoute = Router();
+export const userRoute = Router();
+
 import './authentication/active-sessions.js';
 import './authentication/send-verification-email.js';
 import './authentication/sign-in.js';
@@ -7,10 +15,3 @@ import './dashboard-configuration/put.js';
 import './preferences/put.js';
 import './user/get.js';
 import './user/put.js';
-
-import { Router } from 'express';
-
-export const authenticationRoute = Router();
-export const dashboardConfigurationRoute = Router();
-export const preferencesRoute = Router();
-export const userRoute = Router();
